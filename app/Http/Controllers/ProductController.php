bd9adc2e23a37d51e5f7086c5343623cca6316d8
Abdulmuaz Alshaikhli <slmnshukur1993@gmail.com>
2025-03-26 18:25:43 +0100

    Initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Product::orderBy('sort_order')->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'vat' => 'required|numeric',
+        ]);
+
+        $product = Product::create($request->all());
+        return response()->json($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return response()->json($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->all());
+        return response()->json($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json(null, 204);
+    }
+
+    public function updateSortOrder(Request $request)
+    {
+        foreach ($request->products as $product) {
+            Product::where('id', $product['id'])->update(['sort_order' => $product['sort_order']]);
+        }
+        return response()->json(['message' => 'Sort order updated']);
+    }
+}
